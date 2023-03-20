a77cabc75883291f174f281410315d4e08bdc585
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-03-20 18:28:42 +1100

    initial version complete

--- spoof mac address.py
+++ spoof mac address.py
@@ -2,58 +2,46 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Mar 20 14:42:18 2023
-@author: user
+@author: David Poole
 """
+from sh import grep, ifconfig, sudo, networksetup
+from random import randint
 
-# import subprocess
+# get current MAC address
+# ifconfig en0 | grep ether
+cur = grep(ifconfig("en0"), "ether").strip()
+print("Current MAC Address: ", cur)
 
-# result = subprocess.call(["sudo","/usr/local/bin/airport", "-z"])
-# result = subprocess.check_output(['sudo', '/sbin/ifconfig', "en0"])
-# print(result)
+# generate a new random MAC address (note: may not be compliant)
+mac = [0x60, 0xf8, 0x1d,
+       randint(0x00, 0x7f),
+       randint(0x00, 0x7f),
+       randint(0x00, 0x7f)]
+gen = ':'.join(map(lambda x: "%02x" % x, mac))
 
-from sh import ifconfig
+# disconnect from Wi-Fi network
+# note: for sudo to work without password, add to /etc/sudoers
+# sudo airport -z
+sudo("airport", "-z")
 
-print(ifconfig("en0"))
+# spoof the mac address
+# see note re: /etc/sudoers above
+# sh.sudo("ifconfig", "en0", "ether", gen)
 
+# check it updated
+check = grep(ifconfig("en0"), "ether").strip()
+print("New MAC Address:     ", check)
 
-## get current MAC address
-## generate a new random MAC address
-## disconnect from Wifi network
-## change to the new random MAC address
-## verify it changed
-## Connect to Wifi network
+# check if connected to a Wi-Fi network
+# TODO: maybe change it so it's not an infinite loop XD
+wifi = str(networksetup("-getairportnetwork", "en0"))
+while "NET4" not in wifi:
+    print("Trying to connect to NET4...")
+    networksetup("-setairportnetwork", "en0", "NET4")
+    wifi = str(networksetup("-getairportnetwork", "en0"))
 
-
-"""
-#!/bin/sh
-
-##  
-echo Updating MAC address...
-
-## do until macaddr actually updates
-while [[ $new = $cur ]]
-   do
-      ## disconnect from current network
-      sudo airport -z
-
-      ## get the current mac address
-      cur=$(ifconfig en0 | grep ether)
-      
-      ## generate a new random mac address
-      gen=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
-      
-      ## spoof the macaddr with the generated one
-      sudo ifconfig en0 ether $gen
-
-      ## check the macaddr updated
-      new=$(ifconfig en0 | grep ether)
-   done
-
-echo Waiting...
-sleep 1
-
-## attempt to connect to hotspot
-echo Trying to connect to NET4...
-networksetup -setairportnetwork en0 NET4
-echo Done.
-"""
+# final check and confirmation message to console
+if "NET4" in wifi:
+    print("Connected to NET4.")
+else:
+    print("Error, didn't connect to NET4.")
