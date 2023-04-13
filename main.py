d1eeb94f9d3abaf307499a2d14f8e4db924a57c9
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-04-13 18:10:45 +1000

    Add a function for sending output to the console, with colours

--- main.py
+++ main.py
@@ -8,6 +8,32 @@ from random import randint
 debug_level = False
 
 
+def console_log(colour, scope, message):
+    # ANSI escape sequences for text colours
+    dark_red = '\u001B[31m'
+    dark_green = '\u001B[32m'
+    dark_yellow = '\u001B[33m'
+    default = '\u001B[0m'
+
+    if debug_level is False and colour == "debug":
+        return
+
+    print(default, end="")
+    print(time_stamp(), end="")
+
+    if colour == "warning":
+        print(dark_red, end="")
+    elif colour == "debug":
+        print(dark_yellow, end="")
+    elif colour == "success":
+        print(dark_green, end="")
+
+    print(" [", scope, "] ", sep="", end="")
+
+    print(message, end="")
+    print(default)
+
+
 def time_stamp():
     now = datetime.now()
     return now.strftime("%H:%M:%S")
@@ -19,7 +45,6 @@ def get_mac_address() -> str:
 
 
 def generate_mac_address() -> str:
-
     mac = [0x60, 0xf8, 0x1d,
            randint(0x00, 0x7f),
            randint(0x00, 0x7f),
@@ -33,7 +58,9 @@ def check_wifi(ssid: str) -> bool:
     if ssid in wifi_response:
         min_rate = 7
         while not get_tx_rate() > min_rate:
-            print(time_stamp(), "[check_wifi] lastTxRate <= {int}. Check again in 5s...".format(int=min_rate))
+            console_log("warning",
+                        "check_wifi",
+                        "lastTxRate <= {int}. Check again in 5s...".format(int=min_rate))
             sleep(5)
         return True
     else:
@@ -42,12 +69,14 @@ def check_wifi(ssid: str) -> bool:
 
 def connect_wifi(ssid: str) -> bool:
     for attempt in range(1, 99):
-        print(time_stamp(), "[connect_wifi] Attempt {number} to connect to {ssid}".format(
-            number=attempt,
-            ssid=ssid))
+        console_log("debug",
+                    "connect_wifi",
+                    "Attempt {number} to connect to {ssid}".format(
+                        number=attempt,
+                        ssid=ssid))
         networksetup("-setairportnetwork", "en0", ssid)
         if check_wifi(ssid):
-            print(time_stamp(), "[connect_wifi] Connected to", ssid)
+            console_log("success", "connect_wifi", "Connected to " + ssid)
             sleep(5)
             return True
         sleep(10)
@@ -58,7 +87,7 @@ def spoof_mac():
     # get current MAC address
     # ifconfig en0 | grep ether
     cur = get_mac_address()
-    print(time_stamp(), "[spoof_mac] Current MAC Address: ", cur)
+    console_log("notify", "spoof_mac", "Current MAC Address: " + cur)
 
     # generate a new random MAC address (note: may not be compliant)
     gen = generate_mac_address()
@@ -74,7 +103,7 @@ def spoof_mac():
 
     # check it updated
     check = get_mac_address()
-    print(time_stamp(), "[spoof_mac] New MAC Address:     ", check)
+    console_log("success", "spoof_mac", "New MAC Address: " + check)
 
 
 def get_remaining(logged_in_soup) -> (float, str):
@@ -90,7 +119,7 @@ def kill_cna():
     from sh import pkill
     try:
         result = pkill("-l", "-f", "Captive Network Assistant")
-        print(time_stamp(), "[kill_cna] Killed CNA.", result)
+        console_log("success", "kill_cna", "Killed CNA." + str(result))
         sleep(1)
     except:
         # do nothing
@@ -98,22 +127,23 @@ def kill_cna():
         return 0
 
 
-def try_connect_to(host):
+def try_connect_to(host: str):
     # Keep trying until Google is reached or redirected to hotspot
     # could use ping instead?
     zero_if_connected = 1
     while zero_if_connected > 0:
-        if debug_level: print(time_stamp(), "[try_connect_to] trying", host, "attempt:", zero_if_connected)
+        console_log("debug", "try_connect_to", "trying " + host + " attempt: " + str(zero_if_connected))
         kill_cna()
         try:
             request = requests.get(host)
             if request.status_code == 200:
-                if debug_level: print(time_stamp(), "[try_connect_to]", request.status_code, request.reason,
-                                      request.request.url)
+                console_log("debug",
+                            "try_connect_to",
+                            "connection successful")
                 zero_if_connected = 0
                 return request
         except:
-            print(time_stamp(), "[try_connect_to] connection to", host, "failed.")
+            console_log("warning", "try_connect_to", "connection to " + host + " failed.")
         sleep(5)
 
 
@@ -127,7 +157,7 @@ def get_tx_rate() -> int:
         split = line.strip().split(":")
         results_dict[split[0].strip()] = split[1].strip()
     last_tx_rate = int(results_dict["lastTxRate"])
-    if debug_level: print(time_stamp(), "[get_tx_rate] lastTxRate: ", last_tx_rate)
+    console_log("debug", "get_tx_rate", "lastTxRate: " + str(last_tx_rate))
     return last_tx_rate
 
 
@@ -157,14 +187,14 @@ for i in range(1, 1440):
             remaining_value, remaining_unit = get_remaining(u_soup)
             if remaining_value < 25:
                 # not much though, so spoof a new mac and reconnect
-                print(time_stamp(), "[Logged in] There's only {value} {units} left.".format(
+                console_log("notify", "Logged in", "There's only {value} {units} left.".format(
                     value=remaining_value,
                     units=remaining_unit))
                 spoof_mac()
                 sleep(5)
             else:
                 # enough data left, so just sleep for 60 seconds
-                print(time_stamp(), "[Logged in] There's {value} {units} left.".format(
+                console_log("notify", "Logged in", "There's {value} {units} left.".format(
                     value=remaining_value,
                     units=remaining_unit))
                 sleep(60)
@@ -176,7 +206,7 @@ for i in range(1, 1440):
         s = try_connect_to(actual_url)
 
         if 'Login failed : Sorry, but you are out of data!' in s.text:
-            print(time_stamp(), "[Logged in] Out of data, spoof the mac...")
+            console_log("notify", "Logged in", "Sorry, but you are out of data! Spoofing the mac...")
             spoof_mac()
         else:
             # must be a new mac, so complete the login process
